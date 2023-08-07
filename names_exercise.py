91576ca81875c60599a84c2e01a97d5dd7ba0b16
BirchAD <birch.fish@gmail.com>
2023-08-07 10:27:43 +0100

    Added names exercise Python script

---
+++ names_exercise.py
@@ -0,0 +1,19 @@
+def print_names(names):
+    if len(names) == 0:
+        print("Please add your name.")
+    elif len(names) == 1:
+        print(f"Hello, my name is {names[0]}.")
+    else:
+        print("List of names for everyone in cohort 11:")
+        for name in names:
+            print(f"Hello, my name is {name}.")
+
+def main():
+    names = [
+        # Add name here
+    ]
+
+    print_names(names)
+
+if __name__ == "__main__":
+    main()
